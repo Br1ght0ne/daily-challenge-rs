41b7dd0d4ca56a4ab3f0e73127789ed3a7e498cb
Oleksii Filonenko <brightone@protonmail.com>
2019-09-10 22:21:01 +0300

    Add an explanation comment to Day 56 solution

--- src/day_56_coffee_shop.rs
+++ src/day_56_coffee_shop.rs
@@ -1,5 +1,9 @@
 fn coffee(change: f64) -> String {
     let drink_type = |change: f64| {
+        // Why no `match` statements?
+        // Well, this was the case at the time of writing:
+        // - floating-point patterns were unstable
+        // TODO: revisit this when float pattern are stable
         if change == 2.2 {
             return Some("Americano");
         }
