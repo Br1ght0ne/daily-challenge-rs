21dfe57dae0d3d6a87543ecdfac6c002316fd0e0
Oleksii Filonenko <brightone@protonmail.com>
2019-09-10 22:18:51 +0300

    Fix Day 56

--- src/day_56_coffee_shop.rs
+++ src/day_56_coffee_shop.rs
@@ -1,9 +1,25 @@
-fn coffee(_change: f64) -> String {
-    // TODO: f64 is not Eq or Hash
-    // Thus can't use HashMap.
-    // Also, no matching on floats in match arms.
-    // Seems like it should be skipped for now.
-    unimplemented!()
+fn coffee(change: f64) -> String {
+    let drink_type = |change: f64| {
+        if change == 2.2 {
+            return Some("Americano");
+        }
+        if change == 2.3 {
+            return Some("Latte");
+        }
+        if change == 2.4 {
+            return Some("Flat white");
+        }
+        if change == 3.5 {
+            return Some("Filter");
+        }
+        None
+    };
+
+    if let Some(drink) = drink_type(change) {
+        format!("Here is your {}, have a nice day!", drink)
+    } else {
+        String::from("Sorry, exact change only, try again tomorrow!")
+    }
 }
 
 #[cfg(test)]
@@ -11,13 +27,11 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_latte() {
-        assert_eq!("Here is your Latte, have a nice day!", coffee(2.2));
+        assert_eq!("Here is your Latte, have a nice day!", coffee(2.3));
     }
 
     #[test]
-    #[ignore]
     fn test_surplus() {
         assert_eq!("Sorry, exact change only, try again tomorrow!", coffee(2.7));
     }
